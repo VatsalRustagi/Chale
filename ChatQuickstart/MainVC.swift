fcc3337b59751637914f63028b44a741503f54df
Vatsal Rustagi <vatsalr23@gmail.com>
2017-10-21 20:43:16 -0700

    First Commit

---
+++ ChatQuickstart/MainVC.swift
@@ -0,0 +1,27 @@
+//
+//  MainVC.swift
+//  ChatQuickstart
+//
+//  Created by Vatsal Rustagi on 10/21/17.
+//  Copyright © 2017 Twilio, Inc. All rights reserved.
+//
+
+import UIKit
+
+var userName = ""
+
+class MainVC: UIViewController {
+
+    @IBOutlet weak var enterButton: UIButton!
+    @IBOutlet weak var userTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func pressedEnter(_ sender: UIButton) {
+        userName = userTextField.text!
+        performSegue(withIdentifier: "goToHome", sender: nil)
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,6 @@
+source 'https://github.com/CocoaPods/Specs'
+
+target 'ChatQuickstart' do
+  pod 'TwilioChatClient', '1.0.4'
+end
+
